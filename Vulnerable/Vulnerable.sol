d712b369476741175deb504f402c0ab7e3c4be35
skyge <18844062006@163.com>
2019-04-02 12:38:35 +0800

    A Add the third vulnerable case.

--- Vulnerable/Vulnerable.sol
+++ Vulnerable/Vulnerable.sol
@@ -42,3 +42,21 @@ contract SimpleToken{
         balances[_recipient]+=_amount;
     }  
 }
+
+//*** Exercice 3 ***//
+contract VoteTwoChoices{
+    mapping(address => uint) public votingRights;
+    mapping(address => uint) public votesCast;
+    mapping(bytes32 => uint) public votesReceived;
+    
+    function buyVotingRights() payable {
+        votingRights[msg.sender]+=msg.value/(1 ether);
+    }
+    
+    function vote(uint _nbVotes, bytes32 _proposition) {
+        require(_nbVotes + votesCast[msg.sender]<=votingRights[msg.sender]); // Check you have enough voting rights.
+        
+        votesCast[msg.sender]+=_nbVotes;
+        votesReceived[_proposition]+=_nbVotes;
+    }
+}
