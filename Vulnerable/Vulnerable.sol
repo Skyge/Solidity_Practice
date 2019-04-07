9fdff205d7e9fec7f170d18db4a80eb432462ff0
skyge <18844062006@163.com>
2019-04-07 23:03:59 +0800

    A Add a new vulnerable contract.

--- Vulnerable/Vulnerable.sol
+++ Vulnerable/Vulnerable.sol
@@ -60,3 +60,22 @@ contract VoteTwoChoices{
         votesReceived[_proposition]+=_nbVotes;
     }
 }
+
+//*** Exercice 4 ***//
+contract BuyToken {
+    mapping(address => uint) public balances;
+    uint public price=1;
+    address public owner=msg.sender;
+    
+    function buyToken(uint _amount, uint _price) payable {
+        require(_price>=price);
+        require(_price * _amount * 1 ether <= msg.value);
+        balances[msg.sender]+=_amount;
+    }
+    
+    function setPrice(uint _price) {
+        require(msg.sender==owner);
+        
+        price=_price;
+    }
+}
