a275817bcae86547cefd4b561e59e2bf6f0be67f
skyge <18844062006@163.com>
2019-04-14 22:29:11 +0800

    A Add a new vulnerable contract.

--- Vulnerable/Vulnerable.sol
+++ Vulnerable/Vulnerable.sol
@@ -103,3 +103,28 @@ contract CountContribution{
         totalContributions += _amount;
     }
 }
+
+//*** Exercice 6 ***//
+contract Store {
+    struct Safe {
+        address owner;
+        uint amount;
+    }
+    
+    Safe[] public safes;
+    
+    function store() public payable {
+        safes.push(Safe({owner: msg.sender, amount: msg.value}));
+    }
+    
+    /// @dev Take back all the amount stored.
+    function take() public {
+        for (uint i; i<safes.length; ++i) {
+            Safe storage safe = safes[i];
+            if (safe.owner==msg.sender && safe.amount!=0) {
+                msg.sender.transfer(safe.amount);
+                safe.amount=0;
+            }
+        }
+    }
+}
