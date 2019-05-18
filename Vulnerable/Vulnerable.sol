eb826bbb2c4b966232e8bd9666f51ee8b834898b
skyge <18844062006@163.com>
2019-05-18 23:24:52 +0800

    A Add a new case.

--- Vulnerable/Vulnerable.sol
+++ Vulnerable/Vulnerable.sol
@@ -157,3 +157,22 @@ contract Token {
         balances[msg.sender]=0;
     }  
 }
+
+//*** Exercice 8 ***//
+contract DiscountedBuy {
+    uint public basePrice = 1 ether;
+    mapping (address => uint) public objectBought;
+
+    /// @dev Buy an object.
+    function buy() payable {
+        require(msg.value * (1 + objectBought[msg.sender]) == basePrice);
+        objectBought[msg.sender]+=1;
+    }
+    
+    /** @dev Return the price you'll need to pay.
+     *  @return price The amount you need to pay in wei.
+     */
+    function price() constant returns(uint price) {
+        return basePrice/(1 + objectBought[msg.sender]);
+    }   
+}
