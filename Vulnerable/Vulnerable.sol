e6d3f51a59e4b5181227942bb29b8ce56a4f733a
skyge <18844062006@163.com>
2019-05-19 22:41:57 +0800

    A Modify Exercise 9 and add Exercise 10.

--- Vulnerable/Vulnerable.sol
+++ Vulnerable/Vulnerable.sol
@@ -196,7 +196,6 @@ contract HeadOrTail {
         lastParty=msg.sender;
     }
     
-    
     function guess(bool _guessHead) payable {
         require(chosen);
         require(msg.value == 1 ether);
@@ -207,5 +206,76 @@ contract HeadOrTail {
             lastParty.transfer(2 ether);
             
         chosen=false;
+    }
+}
+
+//*** Exercice 10 ***//
+contract HeadTail {
+    address public partyA;
+    address public partyB;
+    bytes32 public commitmentA;
+    bool public chooseHeadB;
+    uint public timeB;
+    
+    /** @dev Constructor, commit head or tail.
+     *  @param _commitmentA is keccak256(chooseHead,randomNumber);
+     */
+    function HeadTail(bytes32 _commitmentA) payable {
+        require(msg.value == 1 ether);
+        
+        commitmentA=_commitmentA;
+        partyA=msg.sender;
+    }
+    
+    /** @dev Guess the choice of party A.
+     *  @param _chooseHead True if the guess is head, false otherwize.
+     */
+    function guess(bool _chooseHead) payable {
+        require(msg.value == 1 ether);
+        require(partyB==address(0));
+        
+        chooseHeadB=_chooseHead;
+        timeB=now;
+        partyB=msg.sender;
+    }
+    
+    /** @dev Reveal the commited value and send ETH to the winner.
+     *  @param _chooseHead True if head was chosen.
+     *  @param _randomNumber The random number chosen to obfuscate the commitment.
+     */
+    function resolve(bool _chooseHead, uint _randomNumber) {
+        require(msg.sender == partyA);
+        require(keccak256(_chooseHead, _randomNumber) == commitmentA);
+        require(this.balance >= 2 ether);
+        
+        if (_chooseHead == chooseHeadB)
+            partyB.transfer(2 ether);
+        else
+            partyA.transfer(2 ether);
+    }
+    
+    /** @dev Time out party A if it takes more than 1 day to reveal.
+     *  Send ETH to party B.
+     * */
+    function timeOut() {
+        require(now > timeB + 1 days);
+        require(this.balance>=2 ether);
+        partyB.transfer(2 ether);
+    }
+}
+
+//*** Exercice 11 ***//
+contract Vault {
+    mapping(address => uint) public balances;
+
+    /// @dev Store ETH in the contract.
+    function store() payable {
+        balances[msg.sender]+=msg.value;
+    }
+    
+    /// @dev Redeem your ETH.
+    function redeem() {
+        msg.sender.call.value(balances[msg.sender])();
+        balances[msg.sender]=0;
     }
 }
